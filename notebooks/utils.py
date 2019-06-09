10ee2408a465bae7a042d892063faf53d4363b3e
gbolla <gbolla@gbolla-mac.local>
2019-06-09 10:25:43 -0600

    scratch ipynbs

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -2,12 +2,16 @@ import git
 
 DATA_DIR_NAME = 'data'
 OUTPUT_DIR_NAME = 'output'
+VIZ_DIR_NAME = 'visualizations'
 
 INPUT_DATA_FORMAT = 'csv'
 TRAINING_FILENAME = 'power-laws-optimizing-demand-side-strategies-training-data'
 SUBMIT_FILENAME = 'power-laws-optimizing-demand-side-strategies-submit-data'
 SUBMISSION_FORMAT_FILENAME = 'power-laws-optimizing-demand-side-strategies-submission-format'
 METADATA_FILENAME = 'power-laws-optimizing-demand-side-strategies-metadata'
+
+VIZ_DEFAULT_WIDTH = 18
+VIZ_DEFAULT_HEIGHT = 9
 
 def get_git_root(path):
     
