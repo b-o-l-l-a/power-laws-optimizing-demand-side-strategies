569397b3de98526ed2166b80b7a403bf896d3cd6
gbolla <gbolla@gbolla-mac.local>
2019-07-23 07:24:35 -0600

    messy, still retning .5

--- simulate/battery_controller.py
+++ simulate/battery_controller.py
@@ -1,7 +1,6 @@
-# ==============================================================================
-#    THIS CLASS WILL BE IMPLEMENTED BY COMPETITORS
-# ==============================================================================
-class BatteryContoller(object):
+from ortools.linear_solver import pywraplp
+
+class BatteryController(object):
     """ The BatteryContoller class handles providing a new "target state of charge"
         at each time step.
         This class is instantiated by the simulation script, and it can
@@ -26,6 +25,9 @@ class BatteryContoller(object):
           96 quarters (i.e., an array of 96 values).
         :returns: proposed state of charge, a float between 0 (empty) and 1 (full).
     """
+    
+    step = 960
+    
     def propose_state_of_charge(self,
                                 site_id,
                                 timestamp,
@@ -39,27 +41,105 @@ class BatteryContoller(object):
 
         # return the proposed state of charge ...
         
-        print("--------- timestamp")
-        print(timestamp)
-        print("--------- battery")
-        print("-- current_charge: {}".format(battery.current_charge))
-        print("-- capacity: {}".format(battery.capacity))
-        print("-- charging_power_limit: {}".format(battery.charging_power_limit))
-        print("-- discharging_power_limit: {}".format(battery.discharging_power_limit))
-        print("-- charging_efficiency: {}".format(battery.charging_efficiency))
-        print("-- discharging_efficiency: {}".format(battery.discharging_efficiency))
-        print("--------- actual_previous_load")
-        print(actual_previous_load)
-        print("--------- actual_previous_pv_production")
-        print(actual_previous_pv_production)
-        print("--------- price_buy")
-        print(price_buy)
-        print("--------- price_sell")
-        print(price_sell)
-        print("--------- load_forecast")
-        print(load_forecast)
-        print("--------- pv_forecast")
-        print(pv_forecast)
-        raise ValueError('')
+        # return the proposed state of charge ...
+
+        self.step -= 1
+        if (self.step == 1): return 0
+        if (self.step > 1): number_step = min(96, self.step)
+            
+        print(timestamp, self.step)
+        print("actual_previous_load", actual_previous_load)
+        print("actual_previous_pv_production", actual_previous_pv_production)
+
+        price_buy = price_buy.tolist()
+        price_sell = price_sell.tolist()
+        load_forecast = load_forecast.tolist()
+        pv_forecast = pv_forecast.tolist() 
+
+        #battery
+#         print("battery.capacity", battery.capacity)
+#         print("battery.charging_efficiency", battery.charging_efficiency)
+#         print("battery.discharging_efficiency", battery.discharging_efficiency)
+#         print("1. / battery.discharging_efficiency", 1. / battery.discharging_efficiency)
+#         print("(current) capacity * battery.current_charge", battery.capacity * battery.current_charge)
+#         print("battery.charging_power_limit", battery.charging_power_limit)
+#         print("battery.discharging_power_limit", battery.discharging_power_limit)
+#         print("battery.current_charge", battery.current_charge)       
         
-        return 1.0
+        battery_capacity = battery.capacity
+        charging_efficiency = battery.charging_efficiency
+        discharging_efficiency = battery.discharging_efficiency
+        current = battery_capacity * battery.current_charge 
+        print("current", current)
+        charge_limit = battery.charging_power_limit
+        discharge_limit = battery.discharging_power_limit
+        charge_limit /= 4.
+        discharge_limit /= 4.
+
+
+        # Ortools
+        solver = pywraplp.Solver("BatteryCharge", pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+         
+        # charge: how much PV to use to charge battery at given time step
+        charge = [solver.NumVar(0.0, charge_limit, "charge_{}".format(i)) for i in range(number_step)] 
+        
+        # discharge: how much energy from battery to use meet load restrictions
+        discharge = [solver.NumVar( discharge_limit, 0.0, "discharge_{}".format(i)) for i in range(number_step)]
+        
+        # battery_power: amount of power a battery has for each timestep
+        battery_power = [solver.NumVar(0.0, battery_capacity, "battery_charge_{}".format(i)) for i in range(number_step + 1)]
+        
+        # grid_energy: amount of energy to get from grid at each timestep
+        grid_energy = [solver.NumVar(0.0, solver.infinity(), "grid_{}".format(i)) for i in range(number_step)] 
+        
+        # energy_reqd: amount of surplus/deficit energy at a specific timestep, depending on actual load / pv values
+        energy_reqd = [None] * number_step
+
+        for i in range(number_step):
+            energy_reqd[i] = load_forecast[i] - pv_forecast[i]
+            
+        #energy_to_buy = [solver.NumVar(0.0, solver.infinity(), "energy_to_buy_{}".format(i)) for i in range(number_step)] 
+        energy_to_sell = [solver.NumVar(discharge_limit, 0, "energy_to_sell_{}".format(i)) for i in range(number_step)]
+        
+        #Objective function
+        objective = solver.Objective()
+
+        for i in range(number_step):
+            objective.SetCoefficient(grid_energy[i], price_buy[i] - price_sell[i])
+            objective.SetCoefficient(charge[i], price_buy[i])
+            objective.SetCoefficient(discharge[i], -price_sell[i])
+      
+        objective.SetMinimization()
+        
+        grid_constraint = [None] * number_step
+        battery_constraint = [None] * (number_step + 1)
+        battery_constraint[0] = solver.Constraint(current, current)
+        battery_constraint[0].SetCoefficient(battery_power[0], 1)
+        
+        for i in range(0, number_step):
+            
+            grid_constraint[i] = solver.Constraint(energy_reqd[i], solver.infinity())
+            grid_constraint[i].SetCoefficient(grid_energy[i], 1)
+            ##grid_constraint[i].SetCoefficient(energy_to_sell[i], -1)
+            grid_constraint[i].SetCoefficient(charge[i], -1)
+            grid_constraint[i].SetCoefficient(discharge[i], -1)
+            
+            battery_constraint[i + 1] = solver.Constraint(0, 0)
+            battery_constraint[i + 1].SetCoefficient(charge[i], charging_efficiency)
+            battery_constraint[i + 1].SetCoefficient(discharge[i], discharging_efficiency)
+            #battery_constraint[i + 1].SetCoefficient(energy_to_sell[i], discharging_efficiency)
+            battery_constraint[i + 1].SetCoefficient(battery_power[i], 1)
+            battery_constraint[i + 1].SetCoefficient(battery_power[i + 1], -1)
+
+        solver.Solve()
+        
+        print("grid_energy[0]", grid_energy[0].solution_value())
+        print("charge[0]", charge[0].solution_value())
+        if charge[0].solution_value() > 0:
+            raise ValueError('Charge is above 0')
+        print("discharge[0]", discharge[0].solution_value())
+        print("battery_power[i]", battery_power[i].solution_value())
+        print("battery_power[i + 1]", battery_power[i + 1].solution_value())
+        print("\n\n\n")
+
+        return 0.5
